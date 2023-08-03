8ab96abd1152836d965e56e1ab537a0568f8c30d
JustRanm <141078547+JustRanm@users.noreply.github.com>
2023-08-03 00:03:15 +0000

    Ejercicios 3 a 10

--- main.py
+++ main.py
@@ -1,6 +1,75 @@
+'''
 #Hola bro
 #Ejercicio uno
+
+print("Que lo que")
 
 #Ejercion dos
 Universo=("hola mundo")
 print(Universo)
+
+#Ejercicio 3 
+nombre=input("QUE LO QUE BRO, ¿CUAL ES SU NOMBRE?: ")
+print("Un gusto panita " + nombre + " :3")
+
+
+#Ejercicio 4
+print(((3+2)/(2*5))**2)
+
+#ejercicio  5
+horas=float(input("Cuantas horas laburo socio: "))
+precio=float(input("Cuanto le dan por hora: "))
+operacion=str(horas*precio)
+print("Usted mi bro gana " + operacion)
+
+#Tambien se puede usar la coma en vez del str
+
+#Ejercicio 6
+Numero=int(input("Introdusca entero porsitivo: "))
+print("La respuesta es" , Numero*(Numero+1)/2)
+
+#Ejercio 7
+
+P=float(input("Cuantos kg se carga: "))
+E=float(input("Que tan alto es bro, en metros: "))
+Calculo=round(P/E**2 , 2) 
+print("Su indice de masa muscular es" , Calculo)
+
+#Ejercicio 8
+N=int(input("Ponga un numero entero: "))
+M=int(input("Ahora, vuelva y ponga otro entero: "))
+Division=round(N/M,2)
+C=N//M
+R=N%M
+print("La division da: ",Division,", Su conciente es: ",C,"y Su residuo es: ",R,)
+print("""\
+
+    ⣠⣶⡾⠏⠉⠙⠳⢦⡀⠀⠀⠀⢠⠞⠉⠙⠲⡀⠀
+⠀⠀⠀ ⣴⠿⠏⠀⠀⠀⠀⠀⠀⢳⡀⠀⡏⠀⠀⠀⠀⠀⢷
+⠀⠀⢠⣟⣋⡀⢀⣀⣀⡀⠀⣀⡀⣧⠀⢸⠀⠀⠀⠀⠀ ⡇
+⠀⠀⢸⣯⡭⠁⠸⣛⣟⠆⡴⣻⡲⣿⠀⣸⠀⠀OK⠀ ⡇
+⠀⠀⣟⣿⡭⠀⠀⠀⠀⠀⢱⠀⠀⣿⠀⢹⠀⠀⠀⠀⠀ ⡇
+⠀⠀⠙⢿⣯⠄⠀⠀⠀⢀⡀⠀⠀⡿⠀⠀⡇⠀⠀⠀⠀⡼
+⠀⠀⠀⠀⠹⣶⠆⠀⠀⠀⠀⠀⡴⠃⠀⠀⠘⠤⣄⣠⠞⠀
+⠀⠀⠀⠀⠀⢸⣷⡦⢤⡤⢤⣞⣁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢀⣤⣴⣿⣏⠁⠀⠀⠸⣏⢯⣷⣖⣦⡀⠀⠀⠀⠀⠀⠀
+⢀⣾⣽⣿⣿⣿⣿⠛⢲⣶⣾⢉⡷⣿⣿⠵⣿⠀⠀⠀⠀⠀⠀
+⣼⣿⠍⠉⣿⡭⠉⠙⢺⣇⣼⡏⠀⠀⠀⣄⢸⠀⠀⠀⠀⠀⠀
+⣿⣿⣧⣀⣿.........⣀⣰⣏⣘⣆⣀""")  
+
+                        
+
+
+
+#Ejercicio 5
+I=float(input("Cuanta platica va a invertir: "))
+i=float(input("Cual es el porcentaje de interes mi rei: "))
+n=float(input("De cuantos años hablamos: "))
+Formula=I*(i/100+1)
+print("Papi usted de va a ganar una buena platica de:",Formula )
+'''
+#EJERCICIO 10
+p=int(input("Numero de payasos: "))
+m=int(input("Numero de muñecas: "))
+Calculo=(p*112)+(m*75)
+print("El peso total del paquete es : ",Calculo,"gr")
